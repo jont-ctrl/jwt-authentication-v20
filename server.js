84f326e23da7e3ce41bf9de6daab82b39ae53592
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-05-15 12:03:05 +0200

    test bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const pwTest = await bcrypt.hash('testa', 10);
+console.log(pwTest);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
